445f1705f776cab8e426711ea5ee4f3e6ffde9fa
Justas T <j.trimailovas@gmail.com>
2015-08-21 20:58:44 +0300

    Move link construct and scrape into functions

--- parser.py
+++ parser.py
@@ -4,16 +4,37 @@ from bs4 import BeautifulSoup
 
 import requests
 
-link = 'http://auto.plius.lt/skelbimai/naudoti-automobiliai?make_id=43&model_id=193'
-r = requests.get(link)
-r.encoding = 'utf-8'
-html = r.text
 
-soup = BeautifulSoup(html, 'html.parser')
-prices = [price.contents[0] for price in soup.select('p.fl strong')]
-dates = [date.contents[0] for date in soup.select('span[title^=Pagaminimo]')]
+def build_link(manufacturer_id, model_id, page):
+    base_link = 'http://auto.plius.lt/skelbimai/naudoti-automobiliai'
+    manufacturer = 'make_id={}'.format(manufacturer_id)
+    model = 'model_id={}'.format(model_id)
+    page = 'page_nr={}'
 
-z = zip(dates, prices)
+    link = '{base_link}?{manufacturer}&{model}&{page}'.format(
+        base_link=base_link,
+        manufacturer=manufacturer,
+        model=model.format,
+        page=page,
+    )
 
-for i in z:
-    print(i)
+    return link
+
+
+def scrape():
+    r = requests.get(build_link(43, 193, 1))
+    r.encoding = 'utf-8'
+    html = r.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    prices = [price.contents[0] for price in soup.select('p.fl strong')]
+    dates = [date.contents[0] for date in soup.select('span[title^=Pagaminimo]')]
+
+    z = zip(dates, prices)
+
+    for i in z:
+        print(i)
+
+
+if __name__ == '__main__':
+    scrape()
