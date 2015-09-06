e3c6e603a21775e8c5ab7ded8a19775e7d3f5c0e
Justas T <j.trimailovas@gmail.com>
2015-09-06 12:26:48 +0300

    Set limits to min and max years from data
    and set them on exact major ticks for
    better looks.

--- parser.py
+++ parser.py
@@ -105,6 +105,9 @@ def read_csv(maker_name, model_name):
 
 
 def plot(data):
+    # every which year major tick should be placed
+    YEAR_TICK = 3
+
     dates, prices = zip(*data)
     num_dates = []
     # convert dates into numbers which can be plotted
@@ -115,10 +118,33 @@ def plot(data):
         else:
             num_dates.append(datetime.datetime.strptime(date, '%Y').date())
 
+    date_min = min(dates)
+    date_max = max(dates)
+
+    if len(date_min) > 4:
+        year_min = datetime.datetime.strptime(date_min, '%Y-%m').year
+    else:
+        year_min = datetime.datetime.strptime(date_min, '%Y').year
+
+    if len(date_max) > 4:
+        year_max = datetime.datetime.strptime(date_max, '%Y-%m').year + 1
+    else:
+        year_max = datetime.datetime.strptime(date_max, '%Y').year + 1
+
+    # min and max years should be exactly on major tick for better looks
+    year_min = YEAR_TICK * (year_min // YEAR_TICK)
+    year_max = YEAR_TICK * (year_max // YEAR_TICK)
+
     plt.gca().xaxis.set_major_formatter(dt.DateFormatter('%Y'))
-    plt.gca().xaxis.set_major_locator(dt.YearLocator(3))
+    plt.gca().xaxis.set_major_locator(dt.YearLocator(YEAR_TICK))
+
     plt.plot_date(num_dates, prices)
     plt.grid(True)
+
+    plt.gca().set_xlim(
+        datetime.date(year_min, 1, 1),
+        datetime.date(year_max, 1, 1)
+    )
     plt.show()
 
 
